2c22f04f29ba938159c635a2c6fe3d55fa2c8c67
balachaitanya647 <veerapanenibala8@gmail.com>
2026-06-12 18:25:09 +0530

    Updated ALLOWED_HOSTS

--- ev_project/settings.py
+++ ev_project/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-wt!gzewa*!l+qj1pbi)$8e+%)-d$_lk4#z151^se37lj@-h-8w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
